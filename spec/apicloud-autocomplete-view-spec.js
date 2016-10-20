63ecfb0140fd3edeefc3d6c701409168df572524
wei <weiwei@vip.qq.com>
2016-10-20 16:27:49 +0800

    init

---
+++ spec/apicloud-autocomplete-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ApicloudAutocompleteView from '../lib/apicloud-autocomplete-view';
+
+describe('ApicloudAutocompleteView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
